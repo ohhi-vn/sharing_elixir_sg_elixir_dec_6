7acef28ae033f3ea925627f8ccec4d419be776a1
Manh Van Vu <vuvanmanh123@gmail.com>
2023-12-01 17:04:52 +0700

    backup

---
+++ demo_src/distributed/remote_boot_code_loader.exs
@@ -0,0 +1,17 @@
+# start the master node by command
+# iex --sname master --cookie abc123 -S mix
+
+:erl_boot_server.start([{127,0,0,1}])
+
+
+# start the slave node by command
+#  iex --sname worker --cookie abc123 --erl "-hosts 127.0.0.1 -id worker -loader inet"
+
+# run on server
+:rpc.call(:'worker', :code, :add_paths, [:code.get_path])
+
+# check by
+Code.ensure_loaded(MyModule)
+
+# get pid from string.
+:erlang.list_to_pid(String.to_charlist("<0.165.0>"))
---
+++ demo_src/distributed/remote_spawn.exs
@@ -0,0 +1,82 @@
+
+content = quote do
+  defmodule DSpawn do
+    def start() do
+      for node <- Node.list do
+        start(node)
+      end
+    end
+    def start(node) do
+      Node.spawn(node, fn ->
+        IO.puts("#{inspect self()} node: #{inspect Node.self}")
+        receive do
+          {from, :ping} ->
+            send(from, {:pong, {self(), Node.self}})
+          {from, msg}  ->
+            send(from, {:received, {self(), Node.self()}, msg})
+          end
+        end)
+    end
+
+    def start_link() do
+      for node <- Node.list do
+        start_link(node)
+      end
+    end
+    def start_link(node) do
+      Node.spawn_link(node, fn ->
+        IO.puts("#{inspect self()} node: #{inspect Node.self}")
+        receive do
+          {from, :ping} ->
+            send(from, {:pong, {self(), Node.self}})
+          {from, msg}  ->
+            send(from, {:received, {self(), Node.self()}, msg})
+          end
+        end)
+    end
+
+    def start_monitor() do
+      for node <- Node.list do
+        start_monitor(node)
+      end
+    end
+    def start_monitor(node) do
+      Node.spawn(node, fn ->
+        IO.puts("#{inspect self()} node: #{inspect Node.self}")
+        receive do
+          {from, :ping} ->
+            send(from, {:pong, {self(), Node.self}})
+          {from, msg}  ->
+            send(from, {:received, {self(), Node.self()}, msg})
+          msg ->
+            IO.puts "monitor: #{inspect msg}"
+
+          end
+        end)
+    end
+
+    def remote_load_me() do
+      {mod, bin, file} = :code.get_object_code(:"Elixir.DSpawn")
+      for node <- Node.list do
+        remote_load_mod(node, {mod, bin, file})
+      end
+    end
+
+    def remote_load_mod(node, {mod, bin, file}) do
+      :rpc.call(node, :code, :load_binary, [mod, file, bin])
+    end
+
+    def print_me() do
+      IO.puts "#{inspect Node.self()}, #{inspect self()}, #{inspect __MODULE__}"
+    end
+  end
+end
+
+# compile & load for local node
+[{ DSpawn, object_code }] = Code.compile_quoted content
+
+# compile & load for remote node
+:rpc.call(:n2@localhost, Code, :compile_quoted, [content])
+
+# get pid from string.
+:erlang.list_to_pid(String.to_charlist("<0.165.0>"))
